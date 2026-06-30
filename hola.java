2afa9d20beb12dda5df5fb631630f2653341ab3c
Gmr_675 <g58945362@gmail.com>
2026-06-29 18:45:35 -0600

    Add files via upload

---
+++ hola.java
@@ -0,0 +1,12 @@
+
+// Mi primer programa
+
+public class hola {
+
+public static void main(String[] args) {
+
+   System.out.println(" Hola Mundo ");
+   System.out.println("este programa fue elaborado por: \n German mendez");
+
+    }
+    }
