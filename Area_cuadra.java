bb3fd68910240f137e22d79dcb12094d054c6816
Gmr_675 <g58945362@gmail.com>
2026-06-29 18:08:25 -0600

    Add files via upload

---
+++ Area_cuadra.java
@@ -0,0 +1,21 @@
+// Este Programa Muestra El Area de un Rectangulo
+import javax.swing.JOptionPane;
+class Area_cuadra
+{
+public static void main (String args [])
+{
+JOptionPane.showMessageDialog(null,"AREA DE UN RECTANGULO.");
+{
+String a1,b2;
+Double num1,num2,res;
+a1=JOptionPane.showInputDialog("DAME LA BASE");
+b2=JOptionPane.showInputDialog("DAME LA ALTURA");
+num1=Double.parseDouble(a1);
+num2=Double.parseDouble(b2);
+res=num1*num2;
+JOptionPane.showMessageDialog(null," EL AREA \n DEL \n CUADRADO ES:\n "+num1+"*"+num1+" ES = "+res);
+JOptionPane.showMessageDialog(null," ESTE PROGRAMA FUE ELABORADO POR EL PROGRAMADOR; \n GERMAN MENDEZ");
+System.out.println("Fin Del Programa");
+}
+}
+}
