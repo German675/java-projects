b78fed781aadf32c3a9ea2af901b422fe0514b4e
Gmr_675 <g58945362@gmail.com>
2026-06-29 18:45:21 -0600

    Add files via upload

---
+++ bienvenida.java
@@ -0,0 +1,13 @@
+// Programa De Bienvenida
+
+
+public class bienvenida{
+    //el metodo main empieza la ejecucion de la aplicacion en java
+    public static void main(String arg[])
+    {
+
+        System.out.println("BIENVENIDO A LA PROGRAMACION EN JAVA, ESPERO Y TE GUSTE MUCHO");
+        System.out.println("TE DESEO MUCHA SUERTE");
+        System.out.println("PROGRAMA ELABORADO POR EL PROGRAMADOR: GERMAN MENDEZ");
+    }//fin del metodo main
+ }//fin de la clase Bienvenida
